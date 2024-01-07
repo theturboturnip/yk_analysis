bbd05159e03566d1981a4d05b771f7cd15d56244
Samuel Stark <samuel.winslow.stark@gmail.com>
2024-01-07 14:21:42 +0000

    Make renderdoc extension kinda sorta work

--- yk_analysis/renderdoc/__init__.py
+++ yk_analysis/renderdoc/__init__.py
@@ -1,6 +1,6 @@
 import functools
 from hashlib import sha256
-from typing import Any, List, Optional, cast
+from typing import Any, Dict, List, Optional, Set, cast
 import renderdoc as rd
 import qrenderdoc as qrd
 import PySide2.QtWidgets as widgets
@@ -52,6 +52,7 @@ def register(version: str, ctx: qrd.CaptureContext):
 
     # ctx.Extensions().RegisterWindowMenu(qrd.WindowMenu.Window, ["Extension Window"], open_window_callback)
     ctx.Extensions().RegisterWindowMenu(qrd.WindowMenu.Tools, ["Yakuza Scan"], menu_callback)
+    ctx.Extensions().RegisterWindowMenu(qrd.WindowMenu.Tools, ["Yakuza Scan (Debug)"], menu_callback_dbg)
 
 def unregister():
     print("Unregistering my extension")
@@ -105,38 +106,72 @@ def menu_callback(ctx: qrd.CaptureContext, data):
     
     mqt = ctx.Extensions().GetMiniQtHelper()
 
-    ctx.Replay().AsyncInvoke('', lambda r: perform_analysis(ctx, r, mqt, db_path))
+    ctx.Replay().AsyncInvoke('', lambda r: perform_analysis(ctx, r, mqt, db_path, debug=False))
 
-def perform_analysis(ctx: qrd.CaptureContext, r: rd.ReplayController, mqt: qrd.MiniQtHelper, db_path: str):
+def menu_callback_dbg(ctx: qrd.CaptureContext, data):
+    db_path, _selected_filter = widgets.QFileDialog.getOpenFileName(caption="Select Yakuza Game Shader DB Path")
+    if not db_path:
+        ctx.Extensions().MessageDialog("No database selected!", "Extension message")
+        return
+    
+    mqt = ctx.Extensions().GetMiniQtHelper()
+
+    ctx.Replay().AsyncInvoke('', lambda r: perform_analysis(ctx, r, mqt, db_path, debug=True))
+
+def perform_analysis(ctx: qrd.CaptureContext, r: rd.ReplayController, mqt: qrd.MiniQtHelper, db_path: str, debug: bool):
     try:
-        perform_analysis_inner(ctx, r, mqt, db_path)
+        perform_analysis_inner(ctx, r, mqt, db_path, debug=debug)
+        err_msg = None
     except Exception as err:
-        mqt.InvokeOntoUIThread(lambda: ctx.Extensions().MessageDialog(f"{err}", "Extension message"))
+        err_msg = f"{err}"
+    finally:
+        if err_msg:
+            mqt.InvokeOntoUIThread(lambda: ctx.Extensions().ErrorDialog(f"{err_msg}", "Extension message"))
 
-def perform_analysis_inner(ctx: qrd.CaptureContext, r: rd.ReplayController, mqt: qrd.MiniQtHelper, db_path: str):
+def perform_analysis_inner(ctx: qrd.CaptureContext, r: rd.ReplayController, mqt: qrd.MiniQtHelper, db_path: str, debug: bool = False):
+    if debug:
+        cnt = 0
+        dbg_file = open("C:\\Users\\Samuel\\ext_dbg_temp.txt", "a")
+        def print_d(*args):
+            nonlocal cnt
+            cnt += 1
+            dbg_file.write(f"dbg {cnt}: {' '.join(str(a) for a in args)}\n")
+            dbg_file.flush()
+            # mqt.InvokeOntoUIThread(lambda: ctx.Extensions().MessageDialog(f"dbg {cnt}: {s}", "Extension message"))
+    else:
+        def print_d(*args):
+            pass
+
+    print_d("opening db")
     db = ReadOnlyDb(db_path, expected_version=1)
+    print_d("opened db")
 
     def lookup_shader_bytes_in_db(shader_stage: str, bytes_type: str, b: bytes) -> Optional[str]:
         h = sha256()
         h.update(b)
         d = h.digest()
+        print_d("digest", d)
         
         db.cur.execute("SELECT Category, ShaderName FROM ShaderBytes WHERE ShaderStage = ? AND BytesType = ? AND SHA256 = ?", (shader_stage, bytes_type, d, ))
         data = set(
             (str(cat), str(s_name))
             for cat, s_name in db.cur.fetchall()
         )
+        print_d("recalled shaders", data)
         if len(data) == 1:
             cat, s_name = next(iter(data))
             return s_name
         return None
     
-    has_lookedup = set()
+    shader_names: Dict[rd.ResourceId, Optional[str]] = {}
 
     def lookup_shader_in_db(s: rd.D3D11Shader) -> Optional[str]:
-        if s.resourceId in has_lookedup:
-            return ctx.GetResourceNameUnsuffixed(s.resourceId)
+        if s.resourceId in shader_names:
+            return shader_names[s.resourceId]
+        shader_names[s.resourceId] = None
+        print_d("resourceId", s.resourceId)
 
+        print_d("stage", s.stage)
         if s.stage == rd.ShaderStage.Vertex:
             shader_stage = "Vertex"
         elif s.stage == rd.ShaderStage.Pixel:
@@ -144,6 +179,7 @@ def perform_analysis_inner(ctx: qrd.CaptureContext, r: rd.ReplayController, mqt:
         else:
             return None
         
+        print_d("encoding", s.reflection.encoding)
         if s.reflection.encoding == rd.ShaderEncoding.DXBC:
             bytes_type = "DXBC"
         else:
@@ -152,9 +188,13 @@ def perform_analysis_inner(ctx: qrd.CaptureContext, r: rd.ReplayController, mqt:
         name = lookup_shader_bytes_in_db(shader_stage, bytes_type, s.reflection.rawBytes)
         if name is None:
             return None
+        # print_d(name)
+        shader_names[s.resourceId] = name
+
+        print_d(f"@{s.resourceId} {shader_stage} {bytes_type} {s.reflection.rawBytes[:16]} {name}")
         
-        has_lookedup.add(s.resourceId)
-        ctx.SetResourceCustomName(s.resourceId, f"{name} ({shader_stage})")
+        # Don't call this from the replay thread - things break
+        # ctx.SetResourceCustomName(s.resourceId, f"{name} ({shader_stage})")
         return name
 
     # Start iterating from the first real action as a child of markers
@@ -162,19 +202,43 @@ def perform_analysis_inner(ctx: qrd.CaptureContext, r: rd.ReplayController, mqt:
     while len(action.children) > 0:
         action = action.children[0]
 
+    print_d(action)
+
     while action is not None:
+        print_d(f"{action}, {action.flags}")
+
         if action.flags & rd.ActionFlags.Drawcall:
-            r.SetFrameEvent(action.eventId, force=True)
+            r.SetFrameEvent(action.eventId, False) # force=False
+            print_d("set frame event")
             d3d11state = r.GetD3D11PipelineState()
+            print_d(d3d11state.vertexShader)
             vertex_name = lookup_shader_in_db(d3d11state.vertexShader)
             fragment_name = lookup_shader_in_db(d3d11state.pixelShader)
+            print_d("names", vertex_name, fragment_name)
             if vertex_name is None and fragment_name is None:
-                continue
-            if vertex_name == fragment_name:
-                action.customName = f"{vertex_name}"
+                # Don't set a custom name
+                custom_name = None
+            elif vertex_name == fragment_name:
+                custom_name = f"{vertex_name}"
             else:
-                action.customName = f"{vertex_name}/{fragment_name}"
-        
+                custom_name = f"{vertex_name} - {fragment_name}"
+
+            if custom_name is not None:
+                print_d(f"Setting custom name '{custom_name}'")
+                # TODO maybe don't do this from here - the UI thread doesn't uncache them
+                action.customName = custom_name
+
         action = action.next
 
-    mqt.InvokeOntoUIThread(lambda: ctx.Extensions().MessageDialog(f"Done!", "Extension message"))
+    if debug:
+        dbg_file.close()
+
+    def finish():
+        for resourceId, name in shader_names.items():
+            if not name:
+                continue
+            ctx.SetResourceCustomName(resourceId, f"{name}")
+        ctx.Extensions().MessageDialog(f"Done!", "Extension message")
+
+    mqt.InvokeOntoUIThread(finish)
+
